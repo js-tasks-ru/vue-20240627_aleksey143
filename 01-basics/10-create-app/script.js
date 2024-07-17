2b771ec939dddb620fd31a36ed1c615f2e2745c5
Dvoryanchikov <Dvoryanchikov@lanit.ru>
2024-07-17 19:50:50 +0300

    createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,23 @@
 import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    const date = new Date().toLocaleDateString('ru-RU', { dateStyle: 'long' })
+
+    return {
+      date,
+    }
+  },
+
+  template: `
+<div>
+  Сегодня {{date}}
+</div>
+  `,
+})
+
+const app = createApp(App)
+
+app.mount('#app')
