0733f8cf65c2b81a412aa5adfa744ecd790895a2
Dvoryanchikov <Dvoryanchikov@lanit.ru>
2024-08-31 20:07:30 +0300

    03 - Компоненты: погода в Средиземье

---
+++ 03-components/50-weather-components/components/weather-details-item.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'WeatherDetailsItem',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+
+    value: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="weather-details__item">
+    <div class="weather-details__item-label">{{ label }}</div>
+    <div class="weather-details__item-value">{{ value }}</div>
+  </div>
+</template>
---
+++ 03-components/50-weather-components/components/weather-item.ts
@@ -0,0 +1,61 @@
+import { defineComponent, type PropType } from 'vue'
+import { WeatherConditionIcons, type WeatherData } from '../weather.service.ts'
+
+import { convertTime } from '../utils.ts'
+import WeatherDetailsItem from './weather-details-item.vue'
+import WeatherAlert from './weather-alert.vue'
+import WeatherConditions from './weather-conditions.vue'
+
+export default defineComponent({
+  name: 'WeatherItem',
+  components: { WeatherDetailsItem, WeatherAlert, WeatherConditions },
+  props: {
+    item: {
+      type: Object as PropType<WeatherData>,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const alertTitle = `${props.item?.alert?.sender_name}: ${props.item?.alert?.description}`
+
+    const temp = (props.item.current.temp - 273.15).toFixed(1)
+
+    const icon = WeatherConditionIcons[props.item.current.weather.id]
+    const pressure = (props.item.current.pressure * 0.75).toFixed(0)
+
+    const tdMinutes = convertTime(props.item.current.dt)
+
+    const isNight =
+      tdMinutes > convertTime(props.item.current.sunset) && tdMinutes < convertTime(props.item.current.sunrise)
+
+    return {
+      alertTitle,
+      temp,
+      icon,
+      press: pressure,
+      isNight,
+    }
+  },
+
+  template: `
+    <li class="weather-card" :class="{ 'weather-card--night': isNight }">
+      <WeatherAlert v-if="item.alert" :title="alertTitle" />
+      <div>
+        <h2 class="weather-card__name">
+          {{ item.geographic_name }}
+        </h2>
+        <div class="weather-card__time">
+          {{ item.current.dt }}
+        </div>
+      </div>
+      <WeatherConditions :title="item.current.weather.description" :icon="icon" :temp="temp" />
+      <div class="weather-details">
+        <WeatherDetailsItem label="Давление, мм рт. ст." :value="press"/>
+        <WeatherDetailsItem label="Влажность, %" :value="item.current.humidity"/>
+        <WeatherDetailsItem label="Облачность, %" :value="item.current.clouds"/>
+        <WeatherDetailsItem label="Ветер, м/с" :value="item.current.wind_speed"/>
+      </div>
+    </li>
+  `,
+})
