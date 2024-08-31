a91e36bff3927adc8bd720fdd9ddfe5a0c4ce2ce
Dvoryanchikov <Dvoryanchikov@lanit.ru>
2024-08-31 19:01:17 +0300

    03 - UiClock

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -1,9 +1,38 @@
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue'
 
 export default defineComponent({
   name: 'UiClock',
 
-  setup() {},
+  setup() {
+    const showTime = ref(true)
 
-  template: `<div class="clock">10:12:02</div>`,
+    const currentTime = ref('')
+
+    const timerId = ref()
+
+    const createTime = () => {
+      const d = new Date()
+
+      currentTime.value = d.toLocaleTimeString(navigator.language, { timeStyle: 'medium' })
+    }
+
+    onMounted(() => {
+      createTime()
+
+      timerId.value = setInterval(() => {
+        createTime()
+      }, 1000)
+    })
+
+    onUnmounted(() => {
+      clearTimeout(timerId.value)
+    })
+
+    return {
+      showTime,
+      currentTime,
+    }
+  },
+
+  template: `<div v-show="showTime" class="clock">{{ currentTime }}</div>`,
 })
